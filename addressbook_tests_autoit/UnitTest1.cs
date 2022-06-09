31365f7ce4910bcdc89dcfb9f8d16c0e7051a64f
vskAlexR <alexander.rassypin@gmail.com>
2022-06-09 10:28:58 +0300

    Добавьте файлы проекта.

---
+++ addressbook_tests_autoit/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace addressbook_tests_autoit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ addressbook_tests_autoit/appmanager/GroupHelper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace addressbook_tests_autoit
+{
+    public class GroupHelper : HelperBase
+    {
+        public static string GROUPWINTITLE = "Group editor";
+        public static string DELGROUPWINTITLE = "Delete group";
+
+        public GroupHelper(ApplicationManager manager) : base(manager) { }
+
+        public List<GroupData> GetGroupList()
+        {
+            List<GroupData> list = new List<GroupData>();
+            OpenGroupsDialogue();
+            string count = aux.ControlTreeView(GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.2c908d51", "GetItemCount", "#0", "");
+            for (int i = 0; i < int.Parse(count); i++)
+            {
+                string item = aux.ControlTreeView(GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.2c908d51", "GetText", "#0|#" + i, "");
+                list.Add(new GroupData()
+                {
+                    Name = item
+                });
+            }
+            CloseGroupsDialogue();
+            return list;
+        }
+
+        public void Add(GroupData newGroup)
+        {
+            OpenGroupsDialogue();
+            aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d53");
+            aux.Send(newGroup.Name);
+            aux.Send("{ENTER}");
+            CloseGroupsDialogue();
+        }
+
+        private void OpenGroupsDialogue()
+        {
+            aux.ControlClick(WINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d512");
+            aux.WinWait(GROUPWINTITLE);
+        }
+        private void CloseGroupsDialogue()
+        {
+            aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d54");
+        }
+/*        public void Remove(int index)
+        {
+            Window dialogue = OpenGroupsDialogue();
+            Tree tree = dialogue.Get<Tree>("uxAddressTreeView");
+            TreeNode root = tree.Nodes[0];
+            root.GetElement(SearchCriteria.ByControlType(ControlType.TreeItem).AndIndex(index)).SetFocus();
+            dialogue.Get<Button>("uxDeleteAddressButton").Click();
+            Window deldialogue = dialogue.ModalWindow(DELGROUPWINTITLE);
+            deldialogue.Get<RadioButton>("uxDeleteAllRadioButton").Click();
+            deldialogue.Get<Button>("uxOKAddressButton").Click();
+            CloseGroupsDialogue(dialogue);
+        }*/
+    }
+}
